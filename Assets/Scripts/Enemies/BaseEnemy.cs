dd311d178232b51c0a541a880a02843149cc80bf
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-06 18:37:41 -0400

    enemy stuff

---
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : PWPawn
+{
+    public GameObject vision;
+    public float idleWidth = 5f;
+    public float speed = 2f;
+    delegate void currentAction();
+    currentAction currentaction;
+
+    Vector3 startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = gameObject.transform.position;
+        currentaction = Idle;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentaction();
+    }
+    void Idle()
+    {
+
+    }
+    void Chase()
+    {
+
+    }
+    void Attack()
+    {
+        
+    }
+}
