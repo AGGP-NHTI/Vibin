bf3ef8920eab2afe3968fbfe719fc8db9e32edfc
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-07 12:27:56 -0400

    enemy stuff

---
+++ Assets/Scripts/Enemies/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public float damage = 1f;
+    public float LT = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        LT -= Time.fixedDeltaTime;
+        if (LT <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerPawn playerPawn = collider.GetComponent(typeof(PlayerPawn)) as PlayerPawn;
+        if (playerPawn != null)
+        {
+
+        }
+    }
+}
