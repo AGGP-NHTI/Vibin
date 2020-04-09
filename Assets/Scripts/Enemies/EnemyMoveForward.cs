c1660b3a3bdece1cd66ab53253716a8e50e56869
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-09 19:09:42 -0400

    1st enemy

--- Assets/Scripts/Enemies/EnemyMoveForward.cs
+++ Assets/Scripts/Enemies/EnemyMoveForward.cs
@@ -4,38 +4,41 @@ using UnityEngine;
 
 public class EnemyMoveForward : BaseEnemy
 {
-    Rigidbody2D rb;
-    bool direction = false;
-
+    public Rigidbody2D rb;
+    public bool direction = false;
+    Vector3 localScale;
     public enemyhitbox hitbox;
+
+    void Start()
+    {
+        localScale = transform.localScale;
+    }
     void FixedUpdate()
     {
+        transform.localScale = localScale;
         if (direction)
         {
-            Vector3 location = rb.position;
-
-            location += (speed * 1 * Time.fixedDeltaTime * transform.forward);
-
-            rb.position = location;
+            rb.velocity = new Vector3(1, rb.velocity.y, 0);
         }
         else
         {
-            Vector3 location = rb.position;
-
-            location += (speed * -1 * Time.fixedDeltaTime * transform.forward);
-
-            rb.position = location;
+            rb.velocity = new Vector3(-1, 0, 10);
         }
         if (hitbox.hit == true)
         {
             if (direction)
             {
                 direction = false;
+                localScale.x *= -1;
+                hitbox.hit = false;
             }
             else if (!direction)
             {
                 direction = true;
+                localScale.x *= -1;
+                hitbox.hit = false;
             }
+
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
