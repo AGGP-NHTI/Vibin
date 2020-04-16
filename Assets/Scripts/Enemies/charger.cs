44a0ad23aaab267088624785429ce714bd711838
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-16 18:14:01 -0400

    charging enemy

---
+++ Assets/Scripts/Enemies/charger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charger : BaseEnemy
+{
+    public Vision vision;
+    bool charged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (vision.sighted == true)
+        {
+            charged = true;
+        }
+        if (charged)
+        {
+            charge();
+        }
+    }
+    public void charge()
+    {
+        rb.velocity = new Vector3(1, rb.velocity.y, 0);
+    }
+}
