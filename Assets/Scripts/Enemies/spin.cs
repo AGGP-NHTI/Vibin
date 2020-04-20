0551e9337801da2606c1545e19a14c43be58ad3f
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-20 19:26:49 -0400

    boss stuff

---
+++ Assets/Scripts/Enemies/spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    public float RotationRate = 50f;
+    
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(-RotationRate * Time.fixedDeltaTime * Vector3.forward);
+    }
+}
