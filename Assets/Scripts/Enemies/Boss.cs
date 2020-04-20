a6813a63ea30f93b4cb2dc8d80d32b49996d0261
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-20 19:18:11 -0400

    boss stuff

--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class Boss : BaseEnemy
 {
-   
+    delegate void currentAction();
+    currentAction currentaction;
+
+    public EnemyProjectile fire;
+    public float downtime;
+    float currenttime;
+    public GameObject Mouth;
+    public GameObject parent;
+
+    bool FB = false;
+    public GameObject HFirePoint1;
+    public GameObject HFirePoint2;
+    public GameObject SpinPoint;
+    int index = 0;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currenttime = downtime;
+        currentaction = HFire; 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (FB)
+        {
+            EnemyProjectile clone;
+            clone = Instantiate(fire, Mouth.transform.position, Mouth.transform.rotation);
+            clone.transform.SetParent(parent.transform);
+        }
+        currentaction();
+    }
+    public void Idle()
+    {
+        currenttime -= Time.fixedDeltaTime;
+        if (currenttime <= 0)
+        {
+            currenttime = downtime;
+            if (index == 1)
+            {
+                currentaction = HFire;
+            }
+        }
+    }
+
+    public void HFire()
+    {
+        bool finished = false;
+
+        if (finished)
+        {
+            finished = false;
+            currentaction = Idle;
+            index++;
+        }
+    }
+    public void Spin()
+    {
+
     }
 
     public override void GotHit()
