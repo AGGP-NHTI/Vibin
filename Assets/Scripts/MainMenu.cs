7609c4d190ed9ff04dd730b69a5e8440389a99df
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-04-22 14:40:32 -0400

    audio stuff

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    public GameObject Main;
+    public GameObject Options;
+    public Slider slider;
+    void Start()
+    {
+       
+        slider.value = PlayerPrefs.GetFloat("Volume", 0.5f);
+    }
+   
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -19,6 +27,20 @@ public class MainMenu : MonoBehaviour
 #endif
         Debug.Log("Quit");
         Application.Quit();
-
+    }
+    public void ToOptions()
+    {
+        Main.SetActive(false);
+        Options.SetActive(true);
+    }
+    public void ToMain()
+    {
+        Main.SetActive(true);
+        Options.SetActive(false);
+    }
+    public void ChangeVolume()
+    {
+        PlayerPrefs.SetFloat("Volume", slider.value);
+        PlayerPrefs.Save();
     }
 }
