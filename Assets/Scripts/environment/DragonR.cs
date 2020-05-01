980f1cc0ee9c5771d7d71213a550aa156f68ff94
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-05-01 16:12:58 -0400

    boss stuff

---
+++ Assets/Scripts/environment/DragonR.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonR : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip clip;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerPawn playerPawn = collider.GetComponent(typeof(PlayerPawn)) as PlayerPawn;
+
+        if (playerPawn != null)
+        {
+            source.PlayOneShot(clip, PlayerPrefs.GetFloat("Effects"));
+            Destroy(gameObject);
+        }
+    }
+}
