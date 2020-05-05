df1730aff309ed8535e3b4bbe78916c3b935cd73
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-05-05 13:03:28 -0400

    end stuff

---
+++ Assets/Scripts/environment/endmusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endmusic : MonoBehaviour
+{
+    public float length = 7f;
+    public AudioSource first;
+    public AudioSource loop;
+    
+    void Start()
+    {
+        loop.Play();
+        loop.Pause();
+    }
+
+    
+    void Update()
+    {
+        length -= Time.fixedDeltaTime;
+        if (length <= 0)
+        {
+            first.gameObject.SetActive(false);
+            loop.UnPause();
+        }
+    }
+}
