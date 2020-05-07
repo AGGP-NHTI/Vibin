161930363b7084fead57c21db44a837f7b575909
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-05-07 11:30:27 -0400

    animation fixes

---
+++ Assets/Scripts/blink.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blink : MonoBehaviour
+{
+    public PlayerPawn player;
+    public SpriteRenderer renderer;
+    public Color invis;
+    Color normal;
+    public float frequency = 0.1f;
+    float fre;
+    bool on = false;
+
+    void Start()
+    {
+        normal = renderer.color;
+        fre = frequency;
+    }
+    void Update()
+    {
+        if (player.invincible)
+        {
+            flash();
+        }
+        else
+        {
+            renderer.color = normal;
+        }
+    }
+    void flash()
+    {
+        fre -= Time.fixedDeltaTime;
+        if (fre <= 0)
+        {
+            if (on)
+            {
+                renderer.color = invis;
+                on = false;
+            }
+            else if (!on)
+            {
+                renderer.color = normal;
+                on = true;
+            }
+            fre = frequency;
+        }
+    }
+}
