3ac0330b0f3b34d813252ec8b1577accd538da4b
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-05-07 14:18:05 -0400

    fancy ui

---
+++ Assets/Scripts/environment/TitleAnim.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleAnim : MonoBehaviour
+{
+    public GameObject parent;
+    public TitleAnim previous;
+    public bool finished = false;
+    public float rate = 1f;
+    public float delay = 0;
+    public float dc;
+    public bool xreached = false;
+    public bool yreached = false;
+    public Vector3 endscale;
+    public Vector3 startscale;
+    public Vector3 localscale;
+    bool go = false;
+
+    void Start()
+    {
+        dc = delay;
+    }
+    void Awake()
+    {
+        endscale = gameObject.transform.localScale;
+        localscale = startscale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (previous != null)
+        {
+            if (previous.finished == true)
+            {
+                go = true;
+            }
+            else
+            {
+                go = false;
+            }
+        }
+        else
+        {
+            go = true;
+        }
+        if (parent.activeSelf == false)
+        {
+            localscale = startscale;
+        }
+        gameObject.transform.localScale = localscale;
+        if (go)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                if (!xreached)
+                {
+                    localscale += new Vector3(rate, 0, 0);
+                    if (localscale.x >= endscale.x)
+                    {
+                        localscale.x = endscale.x;
+                        xreached = true;
+                    }
+                }
+                if (xreached && !yreached)
+                {
+                    localscale += new Vector3(0, rate, 0);
+                    if (localscale.y >= endscale.y)
+                    {
+                        localscale.y = endscale.y;
+                        yreached = true;
+                        finished = true;
+                        go = false;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/environment/titleparent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titleparent : MonoBehaviour
+{
+    public GameObject parent;
+    public List<TitleAnim> children;
+    
+   
+    void Update()
+    {
+        if (parent.activeInHierarchy == false)
+        {
+            foreach(TitleAnim i in children)
+            {
+                i.gameObject.SetActive(false);
+                i.localscale = i.startscale;
+                i.xreached = false;
+                i.yreached = false;
+                i.finished = false;
+                i.delay = i.dc;
+            }
+        }
+        if (parent.activeInHierarchy != false)
+        {
+            foreach (TitleAnim i in children)
+            {
+                i.gameObject.SetActive(true);
+            }
+        }
+    }
+}
